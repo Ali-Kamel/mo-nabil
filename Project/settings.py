04da484dd78a3ddcfe8a57a1c23ea4a931f8c223
Ali Kamel <ali.kamel9777@yahoo.com>
2019-07-19 16:54:16 +0200

    add

--- Project/settings.py
+++ Project/settings.py
@@ -132,13 +132,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT  = os.path.join(BASE_DIR, "static/")
+# STATIC_ROOT  = os.path.join(BASE_DIR, "static")
+STATIC_ROOT  = '../static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,"Project/media/")
 
-STATICFILE_DIRS = [os.path.join(BASE_DIR, "static_in_env")]
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 
 EMAIL_HOST = 'smtp.epcco.org'
