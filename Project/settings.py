07dd7a95031159f308ca4a38bd6e642144221d61
Ali Kamel <ali.kamel9777@yahoo.com>
2019-07-19 06:03:47 +0200

    change the static dirs

--- Project/settings.py
+++ Project/settings.py
@@ -132,10 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT  = [
-  os.path.join(BASE_DIR, "static"),
-]
-
+STATIC_ROOT  = os.path.join(BASE_DIR, "static"),
+STATICFILE_DIRS = [os.path.join(BASE_DIR, "static_in_env")]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,"Project/media/")
